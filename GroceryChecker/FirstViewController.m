8b033e31f57eba46a8e409ecde47ad04fb6667b8
andy820316 <andylin@andys-macbook-pro.local>
2016-06-22 22:06:28 -0700

    Simple table + data input implemented

--- GroceryChecker/FirstViewController.m
+++ GroceryChecker/FirstViewController.m
@@ -10,13 +10,46 @@
 
 @interface FirstViewController ()
 
+@property (nonatomic,weak) IBOutlet UITextField *inputField;
+@property (nonatomic,weak) IBOutlet UILabel *message;
+@property IBOutlet UITableView *itemList;
+
 @end
 
 @implementation FirstViewController
-
+{
+    NSMutableArray *tableData;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    tableData = [NSMutableArray arrayWithObjects:@"Apples",@"Bananas",@"Carrots", nil];
+    
     // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)Enter {
+    NSString *input = self.inputField.text;
+    if ([input length] == 0){
+        input = @"NULL";
+    }
+    [tableData addObject:input];
+    [self.itemList reloadData];
+    self.message.text = [NSString stringWithFormat:@"%@ was added to the list",input];
+    
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [tableData count];
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc ] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
